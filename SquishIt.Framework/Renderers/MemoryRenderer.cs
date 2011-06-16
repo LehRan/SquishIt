d34d6ed9177c4d7cb8891c2682832c786edd9748
LehRan <LehRan@gmail.com>
2011-06-16 17:57:01 +0300

    Add MemoryRenderer to source control.

---
+++ SquishIt.Framework/Renderers/MemoryRenderer.cs
@@ -0,0 +1,17 @@
+﻿namespace SquishIt.Framework.Renderers
+{
+    public class MemoryRenderer: IRenderer
+    {
+        private string content;
+
+        public void Render(string content, string outputFile)
+        {
+            this.content = content;
+        }
+
+        public string Get()
+        {
+            return this.content;
+        }
+    }
+}
